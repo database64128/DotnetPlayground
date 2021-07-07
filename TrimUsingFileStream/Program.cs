c7fcb99a0704458949945d074dc3473a6dfb0c9a
database64128 <free122448@hotmail.com>
2021-07-07 19:59:55 +0800

    ✂️ Add TrimUsingFileStream

---
+++ TrimUsingFileStream/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TrimUsingFileStream
+{
+    internal class Program
+    {
+        private static Task<int> Main(string[] args)
+        {
+            var s = "Trimming using FileStream causes the error \"Stream does not support writing.\" or \"Cannot access a closed file.\" thrown at runtime.";
+            var testObj = new TestType()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = s,
+            };
+            var rootCommand = new RootCommand(s)
+            {
+                Handler = CommandHandler.Create<CancellationToken>(
+                    async (CancellationToken cancellationToken) =>
+                    {
+                        await using var afs = new FileStream("test-trim-await-using-fs.json", FileMode.Create);
+                        await JsonSerializer.SerializeAsync(afs, testObj, cancellationToken: cancellationToken);
+
+                        using var sfs = new FileStream("test-trim-using-fs.json", FileMode.Create);
+                        await JsonSerializer.SerializeAsync(sfs, testObj, cancellationToken: cancellationToken);
+                    }),
+            };
+
+            Console.OutputEncoding = Encoding.UTF8;
+            return rootCommand.InvokeAsync(args);
+        }
+
+        public static readonly JsonSerializerOptions camelCaseJsonSerializerOptions = new()
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true,
+        };
+    }
+
+    public class TestType
+    {
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+    }
+}
