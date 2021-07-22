aa68d5e1a6b713a9f763bc92f35c1479235d482a
database64128 <free122448@hotmail.com>
2021-07-22 14:50:14 +0800

    💨 Add CommandLinePlayground

---
+++ CommandLinePlayground/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Text;
+
+var pluginOption = new Option<string>("--plugin");
+var pluginVersionOption = new Option<string>("--plugin-version");
+var pluginWhateverOption = new Option<string[]>("--plugin-whatever");
+
+var rootCommand = new RootCommand("")
+{
+    pluginOption,
+    pluginVersionOption,
+    pluginWhateverOption,
+};
+
+rootCommand.Handler = CommandHandler.Create<string, string, string, string[]>(
+    (string plugin, string pluginVersion, string pluginOptions, string[] pluginWhatever) =>
+    {
+        Console.WriteLine($"Plugin: {plugin}");
+        Console.WriteLine($"Plugin Version: {pluginVersion}");
+        Console.WriteLine($"Plugin Whatever: {pluginWhatever.Length}");
+    });
+
+Console.OutputEncoding = Encoding.UTF8;
+await rootCommand.InvokeAsync(args);
