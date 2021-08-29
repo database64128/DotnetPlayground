c3268d78c72b75d67d208eb9a710899c0681c023
database64128 <free122448@hotmail.com>
2021-08-29 20:32:36 +0800

    🔁 Add interactive CLI

--- CommandLinePlayground/Program.cs
+++ CommandLinePlayground/Program.cs
@@ -12,11 +12,16 @@ var pluginWhateverOption = new Option<string[]>("--plugin-whatever")
     AllowMultipleArgumentsPerToken = true,
 };
 
+var interactiveCommand = new Command("interactive", "Enter interactive mode (REPL). Exit with 'exit' or 'quit'.");
+interactiveCommand.AddAlias("i");
+interactiveCommand.AddAlias("repl");
+
 var rootCommand = new RootCommand("")
 {
     pluginOption,
     pluginVersionOption,
     pluginWhateverOption,
+    interactiveCommand,
 };
 
 rootCommand.Handler = CommandHandler.Create<string, string, string, string[]>(
@@ -25,6 +30,27 @@ rootCommand.Handler = CommandHandler.Create<string, string, string, string[]>(
         Console.WriteLine($"Plugin: {plugin}");
         Console.WriteLine($"Plugin Version: {pluginVersion}");
         Console.WriteLine($"Plugin Whatever: {pluginWhatever.Length}");
+    });
+
+interactiveCommand.Handler = CommandHandler.Create(
+    async () =>
+    {
+        while (true)
+        {
+            Console.Write("> ");
+            var inputLine = Console.ReadLine()?.Trim();
+
+            // Verify input
+            if (inputLine is null or "exit" or "quit")
+                break;
+            if (inputLine is "i" or "interactive" or "repl")
+            {
+                Console.WriteLine("🛑 I see what you're trying to do!");
+                continue;
+            }
+
+            await rootCommand.InvokeAsync(inputLine);
+        }
     });
 
 Console.OutputEncoding = Encoding.UTF8;
