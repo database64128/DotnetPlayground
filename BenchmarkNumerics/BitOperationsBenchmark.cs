3ec6f80dfed0e7f8f84a70d3c4971e37e70560ff
database64128 <free122448@hotmail.com>
2022-06-09 15:43:43 +0800

    🔢 Add BenchmarkNumerics

---
+++ BenchmarkNumerics/BitOperationsBenchmark.cs
@@ -0,0 +1,20 @@
+﻿#pragma warning disable CA1822 // Mark members as static
+
+using BenchmarkDotNet.Attributes;
+using System.Numerics;
+
+namespace BenchmarkNumerics;
+
+[MemoryDiagnoser]
+public class BitOperationsBenchmark
+{
+    public const ulong TestNumber = 1UL << 63;
+
+    [Benchmark(Baseline = true)]
+    public void IsPow2() => BitOperations.IsPow2(TestNumber);
+
+    [Benchmark]
+    public void IsPow2Popcnt() => IsPow2PopcntImpl(TestNumber);
+
+    private static bool IsPow2PopcntImpl(ulong ul) => BitOperations.PopCount(ul) == 1;
+}
---
+++ BenchmarkNumerics/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using BenchmarkNumerics;
+
+BenchmarkRunner.Run<BitOperationsBenchmark>();
